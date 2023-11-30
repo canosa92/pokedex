98ab23ddfa0514a5d4ea9442e8463473a4f294f6
Adrian Canosa <101671780+canosa92@users.noreply.github.com>
2023-11-30 12:47:18 +0100

    mas funciones script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
+//llamamos todo lo que nos interesa del HTML
 const input = document.getElementById('searchInput');
 const buscadorSearch = document.getElementById ('searchBtn')
 const prevBtn = document.getElementById('prevBtn');
 const nextBtn = document.getElementById('nextBtn');
 const resetBtn = document.getElementById('resetBtn');
 const visible = document.getElementById('app');
-let paginaIncial= `https://pokeapi.co/api/v2/pokemon?limit=10&offset=0`
+const selector = document.getElementById('selector');
+let paginaIncial =`https://pokeapi.co/api/v2/pokemon?limit=10&offset=0`
 let nextpage;
 let prevpage;
-//let url = "https://pokeapi.co/api/v2/pokemon/"
-"https://pokeapi.co/api/v2/pokemon?offset=10&limit=10"
-​
+
+//creamos la funcion que nos llame al fetch de pokemon
 const ObtenerPokemons = async(pagina) =>{
     try{
         const response = await fetch (pagina);
@@ -18,72 +19,71 @@ const ObtenerPokemons = async(pagina) =>{
             throw new Error( 'ha surgido un error', response.status)
         }
         const data = await response.json();
-​
         nextpage = data.next;
         prevpage = data.previous
         console.log(data)
-        
-​
     pokemonInfo(data)
-​
     }    
     catch (error){
         console.log('error', error)
     }
 }
-ObtenerPokemons(paginaIncial);
-​
-​
+//
+
+
 //hacemos un bucle para hacer una peditcion al fetch y obtener los datos de los pokemon que queremos mostrar
 const pokemonInfo=(data)=>{
     let todos = data.results;
-​
    //for(let i=0; i<=todos.length; i++){
     todos.forEach(element => {
-​
      fetch(element.url)
      .then((response) => response.json())
      .then(data =>{
-         console.log('d',data)
-        
+         console.log(data)
+      
  
-         let name = data.name;
-         let img = data.sprites.front_shiny;
-      console.log('n', name)
+         let name = data.name.toUpperCase();
+         let img = data.sprites.other["official-artwork"].front_default;
+         let id = data.id
+         let type = data.types[0].type.name;
+
+
  
         let infopokemon= `
-         <div class="contenedorPokemon">
-         <h3> ${name}</h3>
-         <img src="${img}" />
+         <div class="contenedorPokemon ${type}">
+         <div class="pokemon-imagen">
+         <img src="${img}"/>
+         </div>
+         <div class="pokemon-info">
+         <h4>${id}.${name}</h4>
+         </div>
+         <div class="texto">
+         <h4>${type}</h4>
+         </div>
              </div>`
              visible.innerHTML += infopokemon
      })
     // }
     });
 }
-​
-​
+
 //boton next
 nextBtn.addEventListener('click', () => {
     visible.innerHTML = '';
     ObtenerPokemons(nextpage)
 })    
-​
-​
+
 //boton previus
 prevBtn.addEventListener('click', () => {
     visible.innerHTML = '';
     ObtenerPokemons(prevpage)
-​
+
 })    
-​
-​
-//buscador
-​
+
+//Creamos un buscador para traer la informacion de un pokemon que querramos obtener
+
 buscadorSearch.addEventListener('click',()=>{
 let search = input.value.toLocaleLowerCase();
-​
-​
  fetch(`https://pokeapi.co/api/v2/pokemon/${search}`)
  .then(response => response.json())
     .then(data => {
@@ -92,19 +92,31 @@ let search = input.value.toLocaleLowerCase();
      
         let name = data.name;
         let img = data.sprites.front_shiny;
-     console.log('n', name)
-​
+     
        let infopokemon= `
         <div class="contenedorPokemon">
-        <h3> ${name}</h3>
         <img src="${img}" />
+        <h3> ${name}</h3>
+        
             </div>`
             visible.innerHTML += infopokemon
         })
 })
-​
+
 //reset
 resetBtn.addEventListener('click', () => {
+    visible.innerHTML =''
     location.reload();
    })
-   
+
+//para seleccionar cuantos pokemons queremos ver en cada pagina
+selector.addEventListener('click',()=>{
+    let limite = selector.value;
+     paginaIncial= `https://pokeapi.co/api/v2/pokemon?limit=${limite}&offset=0`
+     console.log(paginaIncial)
+     visible.innerHTML =''
+     ObtenerPokemons(paginaIncial);
+ })
+
+//llamamos a la funcion de obtenerPokemons para cargarla y que nos aparezaca pokemons al cargar
+ ObtenerPokemons(paginaIncial);
