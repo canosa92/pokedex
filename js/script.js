6a371bedca8aef436f8ff03a9ea0545a3b34df11
Adrian Canosa <101671780+canosa92@users.noreply.github.com>
2023-12-04 20:05:34 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -16,98 +16,94 @@ let prevpage;
 
 
 //creamos la funcion para hacer la paginacion y para traernos el array de los pokemons
-const ObtenerPokemons =(pagina) =>{
-    fetch (pagina)
-    .then(response=>response.json())
-    .then(data =>{
+Tu código muestra una función `ObtenerPokemons` que realiza una solicitud a la PokéAPI para obtener una lista de Pokémon y luego, para cada uno de ellos, llama a la función `pokemonNumero` para obtener información adicional y mostrarla en el HTML.
+
+Aquí hay algunas sugerencias y mejoras para tu código:
+
+1. **Manejo de Promesas:**
+   Aprovecha el uso de `async/await` para manejar las Promesas de manera más limpia y comprensible.
+
+2. **Diferenciación de las Solicitudes:**
+   En tu código actual, realizas dos solicitudes por cada Pokémon: una para obtener detalles generales y otra para obtener detalles específicos de la especie. Puedes manejar ambas solicitudes de manera más eficiente utilizando Promise.all.
+
+3. **Corrección de la Asignación de `infopokemon` a `lista.innerHTML`:**
+   En lugar de asignar `infopokemon` directamente a `lista.innerHTML`, puedes crear un elemento contenedor (`div` en este caso) y establecer su contenido HTML. Esto ayuda a evitar problemas con la representación de cadenas HTML en el DOM.
+
+Aquí tienes una versión modificada de tu código:
+
+```javascript
+const ObtenerPokemons = async (pagina) => {
+    try {
+        const response = await fetch(pagina);
+        const data = await response.json();
         nextpage = data.next;
-        prevpage = data.previous
-        let todos =data.results
-    
-    todos.forEach(todos => {
-      
-    pokemonNumero(todos.name)
-    })
-    })
-}
+        prevpage = data.previous;
+        let todos = data.results;
 
-//hacemos otra peticion fetch para traernos los datos que nos interesa de los pokemons y los ponemos en el HTML
-const pokemonNumero=(nombrePokemon)=>{
-    
-    
-//creamos un bucle con la cantidad de pokemon que nos devuelve la primera peticion
-//con el nombre de los pokemons hacemos una peticion para que nos devuelva las descripciones   y la entrada    
-fetch(`https://pokeapi.co/api/v2/pokemon/${nombrePokemon}`)
-    .then(response =>response.json())
-    .then(pokemon=>{
-    
-pokemonDescripcion = fetch(`https://pokeapi.co/api/v2/pokemon-species/${nombrePokemon}`)
-        .then(response =>response.json())
-        .then (pokemonDescripcion =>{
-            
-//let descripcionEspañol = descripcionPokemon(flavortextentries)
-//para conseguir otros valores que nos interesan
-//const habilidad=fetchDescripcion.abilities
-//const detalles= detalle(habilidad)
+        for (const pokemon of todos) {
+            await pokemonNumero(pokemon.name);
+        }
+    } catch (error) {
+        console.error('Error al obtener la lista de Pokémon:', error);
+    }
+};
 
-        let name = pokemon.name.toUpperCase();
-        let img = pokemon.sprites.other["official-artwork"].front_shiny;
-        let id = pokemon.id
-        let habilidad1 =pokemon.abilities[0].ability.name
-        let type= pokemon.types[0].type.name
-        let types = pokemon.types.map(type =>type.type.name)
-            types =types.join('   ')
-        let hpNumero = pokemon.stats[0].base_stat;
-        let ataqueNumero =pokemon.stats[1].base_stat;
-        let defenseNumero =pokemon.stats[2].base_stat
-        let ataqueSpecialNumero =pokemon.stats[3].base_stat
-        let defensaSpecialNumero =pokemon.stats[4].base_stat
-        let spreedNumero =pokemon.stats[5].base_stat 
-         // <p>${descripcionEspañol}</p>  
-            let lista=document.createElement('li')
-       
-    let infopokemon=`
-         <div class="contenedorPokemon div${type}">
-             <div class="id">
-                 <h4>${id}</h4>
-             </div>
-             <div class="pokemon-imagen">
-                 <img src="${img}"/>
-             </div>
+const pokemonNumero = async (nombrePokemon) => {
+    try {
+        const [pokemon, descripcion] = await Promise.all([
+            fetch(`https://pokeapi.co/api/v2/pokemon/${nombrePokemon}`).then(response => response.json()),
+            fetch(`https://pokeapi.co/api/v2/pokemon-species/${nombrePokemon}`).then(response => response.json())
+        ]);
+
+        // Obtener detalles del Pokemon y la descripción
+        const name = pokemon.name.toUpperCase();
+        const img = pokemon.sprites.other["official-artwork"].front_shiny;
+        const id = pokemon.id;
+        const habilidad1 = pokemon.abilities[0].ability.name;
+        const type = pokemon.types[0].type.name;
+        const types = pokemon.types.map(type => type.type.name).join('   ');
+        const hpNumero = pokemon.stats[0].base_stat;
+        const ataqueNumero = pokemon.stats[1].base_stat;
+        const defenseNumero = pokemon.stats[2].base_stat;
+        const ataqueSpecialNumero = pokemon.stats[3].base_stat;
+        const defensaSpecialNumero = pokemon.stats[4].base_stat;
+        const spreedNumero = pokemon.stats[5].base_stat;
+
+        // Crear un elemento contenedor y establecer su contenido HTML
+        const contenedorPokemon = document.createElement('div');
+        contenedorPokemon.classList.add('contenedorPokemon', `div${type}`);
+        contenedorPokemon.innerHTML = `
+            <div class="id">
+                <h4>${id}</h4>
+            </div>
+            <div class="pokemon-imagen">
+                <img src="${img}"/>
+            </div>
             <div class="pokemon-info">
-                 <h4>${name}</h4>
-                 <h3 class="tipo ${types}">${types}</h3>
-                 <div class="habilidades">
-                     <h4>${habilidad1}</h4>
-                 </div>
-                 <div class="oculto">
-                     <article>
-                         <div class="descripcion">
-                         
-                         </div>
-                         <div class="estadisticas">
-                             <label for="${hpNumero}">HP</label>
-                             <progress id="${hpNumero}" max="100" value="${hpNumero}">${hpNumero}</progress>
-                             <label for="${ataqueNumero}">ATTACK</label>
-                            <progress id="${ataqueNumero}" max="100" value="${ataqueNumero}">${ataqueNumero}</progress>
-                            <label for="${defenseNumero}">DEFENSE</label>
-                             <progress id="${defenseNumero}" max="100" value="${defenseNumero}">${defenseNumero}</progress>
-                             <label for="${ataqueSpecialNumero}">SPECIAL ATTACK</label>
-                             <progress id="${ataqueSpecialNumero}" max="100" value="${ataqueSpecialNumero}">${ataqueSpecialNumero}</progress>
-                             <label for="${defensaSpecialNumero}">SPECIAL DEFENSE</label>
-                             <progress id="${defensaSpecialNumero}" max="100" value="${defensaSpecialNumero}">${defensaSpecialNumero}</progress>
-                             <label for="${spreedNumero}">SPREED</label>
-                             <progress id="${spreedNumero}" max="100" value="${spreedNumero}">${spreedNumero}</progress>
-                     </div>
-                 </article>
-             </div>
-        </div>`
-             lista.innerHTML = infopokemon   
-            visible.appendChild(lista)
-            
- })
-    })
-}
+                <h4>${name}</h4>
+                <h3 class="tipo ${types}">${types}</h3>
+                <div class="habilidades">
+                    <h4>${habilidad1}</h4>
+                </div>
+                <div class="oculto">
+                    <article>
+                        <!-- ... Otras secciones del HTML ... -->
+                    </article>
+                </div>
+            </div>
+        `;
+
+        // Agregar el nuevo Pokémon al contenedor 'visible'
+        visible.appendChild(contenedorPokemon);
+    } catch (error) {
+        console.error('Error al obtener detalles del Pokémon:', error);
+    }
+};
+```
+
+Este código utiliza `async/await` y `Promise.all` para mejorar la legibilidad y el manejo de Promesas. También utiliza un elemento contenedor (`div`) para evitar problemas con la asignación directa de HTML a `innerHTML`. Ajusta según sea necesario para tu aplicación.
+//creamos un bucle con la cantidad de pokemon que nos devuelve la primera peticion
+//con el nombre de los pokemons hacemos una peticion para que nos devuelva las descripciones   y la 
     // 
 function descripcionPokemon(flavortextentries){
    
