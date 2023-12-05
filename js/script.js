9ed0419cb12e5bd18dff8ba1d71fec17ff23aa96
Adrian <adrian>
2023-12-05 22:37:58 +0100

    mejorando css

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ const ObtenerPokemons = async (pagina) => {
     let todos = data.results;
 
     for (const pokemon of todos) {
-     await pokemonNumero(pokemon.name);
+      await pokemonNumero(pokemon.name);
     }
   } catch (error) {
     console.error('Error al obtener la lista de Pokémon:', error);
@@ -42,63 +42,59 @@ const pokemonNumero = async (nombrePokemon) => {
         (response) => response.json()
       ),
     ]);
-    const arrayhabilidades=detalle(pokemon.abilities)
+    const arrayhabilidades = await detalle(pokemon.abilities)
+    const habilidades= arrayhabilidades.map(habilidad =>`<div class="hab"><h4>${habilidad.nombre}</h4><p>${habilidad.descripcion}</p></div>`).join('')
+    console.log(arrayhabilidades)
 
-const descripcionEspañol=descripcionPokemon(descripcion.flavor_text_entries);
+    const descripcionEspañol = descripcionPokemon(descripcion.flavor_text_entries);
     // Obtener detalles del Pokemon y la descripción
     const name = pokemon.name.toUpperCase();
     const img = pokemon.sprites.other['official-artwork'].front_shiny;
     const id = pokemon.id;
-    //const habilidad1 = arrayhabilidades[1].name;
- //<h4>${habilidad1}</h4>
-    const type = pokemon.types[0].type.name;
-    const types = pokemon.types.map((type) => type.type.name).join('   ');
-    const hpNumero = pokemon.stats[0].base_stat;
-    const ataqueNumero = pokemon.stats[1].base_stat;
-    const defenseNumero = pokemon.stats[2].base_stat;
-    const ataqueSpecialNumero = pokemon.stats[3].base_stat;
-    const defensaSpecialNumero = pokemon.stats[4].base_stat;
-    const spreedNumero = pokemon.stats[5].base_stat;
-    const peso = `${pokemon.weight / 10} kg`;
-    const height= `${pokemon.height / 10} mts`;
-
    
+    const type = pokemon.types[0].type.name;
+    const types = pokemon.types.map((type) =>`<h4 class="tipo_texto ${type.type.name}">${type.type.name}<h4>`).join('   ');
+    const estadisticas =pokemon.stats.map((stats)=>`<label for="${stats.stat.name}">${stats.stat.name}</label>
+    <progress id="${stats.stat.name}" max="100" value="${stats.base_stat}">${stats.base_stat}</progress>`).join(' ');
+    const peso = `${pokemon.weight / 10} kg`;
+    const height = `${pokemon.height / 10} mts`;
+
+
 
     // Crear un elemento contenedor y establecer su contenido HTML
     const contenedorPokemon = document.createElement('div');
     contenedorPokemon.classList.add('contenedorPokemon', `div${type}`);
     contenedorPokemon.innerHTML = `
+    <div class="infoBasica">
         <div class="id">
-        <h4>${id}</h4>
+        <h3>${id}</h3>
+        </div>
+        <div class="nombre">
+        <h3>${name}</h3>
+        </div>
     </div>
     <div class="pokemon-imagen">
         <img src="${img}"/>
     </div>
    <div class="pokemon-info">
-        <h4>${name}</h4>
-        <h3 class="tipo ${types}">${types}</h3>
-        <h4> peso: ${peso} <br>altura: ${height}
-        <div class="habilidades">
-           
+        <div class="tipos_Altura">
+        ${types}
         </div>
-        <div class="oculto">
-            <article>
-                <div class="descripcion">
+        <div class="peso_altura">
+        <h4 class="medida">  ${peso}</h4>
+        <h4 class="medida"> ${height}</h4>
+        </div>
+        <div class="descripcion">
                 <p>${descripcionEspañol}</p>
                 </div>
+        <div class="oculto">
+            <article>
+                <div class="habilidades">
+                
+                 ${habilidades}
+                 </div>
                 <div class="estadisticas">
-                    <label for="${hpNumero}">HP</label>
-                    <progress id="${hpNumero}" max="100" value="${hpNumero}">${hpNumero}</progress>
-                    <label for="${ataqueNumero}">ATAQUE</label>
-                   <progress id="${ataqueNumero}" max="100" value="${ataqueNumero}">${ataqueNumero}</progress>
-                   <label for="${defenseNumero}">DEFENSA</label>
-                    <progress id="${defenseNumero}" max="100" value="${defenseNumero}">${defenseNumero}</progress>
-                    <label for="${ataqueSpecialNumero}">ATAQUE ESPECIAL</label>
-                    <progress id="${ataqueSpecialNumero}" max="100" value="${ataqueSpecialNumero}">${ataqueSpecialNumero}</progress>
-                    <label for="${defensaSpecialNumero}">DEFENSA ESPECIAL</label>
-                    <progress id="${defensaSpecialNumero}" max="100" value="${defensaSpecialNumero}">${defensaSpecialNumero}</progress>
-                    <label for="${spreedNumero}">VELOCIDAD</label>
-                    <progress id="${spreedNumero}" max="100" value="${spreedNumero}">${spreedNumero}</progress>
+                    ${estadisticas}
             </div>
         </article>
     </div>
@@ -120,27 +116,26 @@ function descripcionPokemon(flavortextentries) {
     : 'No hay una descripción en español para este Pokémon';
 }
 
-function detalle(habilidad) {
-  let arrayhabilidades = [];
+async function detalle(habilidad) {
+  let arrayhabilidades = await
+  Promise.all(
+    habilidad.map(async (ability) =>{
 
-  habilidad.forEach((ability) => {
     const urlHabilidad = ability.ability.url;
 
-    fetch(`${urlHabilidad}`)
-    .then((response) => response.json())
-    .then((fetchHabilidad) => {
-    
-    const abilityDetails = {
-      nombre: fetchHabilidad.names.find((name) => name.language.name === 'es')
-        .name,
-      descripcion: fetchHabilidad.flavor_text_entries.find(
-        (entry) => entry.language.name === 'es'
-      ).flavor_text,
-    };
-    arrayhabilidades.push(abilityDetails);
-  });
-})
-return arrayhabilidades
+    const response = await fetch(`${urlHabilidad}`)
+    const fetchHabilidad = await response.json()
+  
+        return {
+          nombre: fetchHabilidad.names.find((name) => name.language.name === 'es')
+            .name,
+          descripcion: fetchHabilidad.flavor_text_entries.find(
+            (entry) => entry.language.name === 'es'
+          ).flavor_text,
+        };
+       })
+      );
+  return arrayhabilidades
 }
 
 nextBtn.addEventListener('click', () => {
@@ -162,7 +157,6 @@ buscadorSearch.addEventListener('click', () => {
 selector.addEventListener('click', () => {
   limite = selector.value;
   paginaIncial = `https://pokeapi.co/api/v2/pokemon?limit=${limite}&offset=0`;
-  console.log(paginaIncial);
   visible.innerHTML = '';
   ObtenerPokemons(paginaIncial);
 });
@@ -171,14 +165,13 @@ azar.addEventListener('click', () => {
   visible.innerHTML = '';
   for (let i = 0; i < limite; i++) {
     let random = Math.floor(Math.random() * (1225 - 1 + 1) + 1);
-    console.log(random);
     pokemonNumero(random);
   }
 });
 /*creamos una lista con los distintos nombres de los pokemon, y hacemos un evento para que cuando le den click a los nombres se obtenga
 la infomacion de los pokemons*/
 const nombresPoke = document.getElementById('nombres');
-let listaNombres=[];
+let listaNombres = [];
 fetch('https://pokeapi.co/api/v2/pokemon?limit=1017&offset=0')
   .then((response) => response.json())
   .then((data) => {
@@ -198,8 +191,8 @@ fetch('https://pokeapi.co/api/v2/pokemon?limit=1017&offset=0')
     });
   })
   .catch((error) =>
-    console.error('Error al obtener los nombres de los pokemon')
+    console.error('Error al obtener los nombres de los pokemon', error)
   );
-
+console.log(listaNombres)
 //llamamos a la funcion de obtenerPokemons para cargarla y que nos aparezaca pokemons al cargar
 ObtenerPokemons(paginaIncial);
